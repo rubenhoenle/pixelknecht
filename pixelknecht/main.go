ae6da0baa3f08bbd5c7bce2c5f8110512c80605a
Ruben Hoenle <git@hoenle.xyz>
2024-10-20 17:55:23 +0200

    feat(pixelknecht): allow drawing static images

--- pixelknecht/main.go
+++ pixelknecht/main.go
@@ -4,19 +4,29 @@ import (
 	"context"
 	"crypto/rand"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
 	"io"
 	"log"
 	"net"
 	"net/http"
+	"os"
 	"sync"
 	"time"
-    "encoding/json"
 )
 
 // TODO: check if we can use the struct from the "commanderer" module here instead of duplicating it
 type floodMode struct {
 	Enabled bool `json:"enabled"`
+}
+
+type floodImage struct {
+	Bytes    []string
+	HeightPX int
+	WidthPX  int
 }
 
 var wg sync.WaitGroup
@@ -59,12 +69,12 @@ func commandHandler(conn net.Conn, pollIntervalSec int) {
 		// check if the mode changed in the meantime, if so, react to it
 		if previousMode.Enabled != mode.Enabled {
 			if mode.Enabled {
-                fmt.Print("Starting flooding...")
-	            ctx, cancel = context.WithCancel(context.Background())
+				fmt.Print("Starting flooding...")
+				ctx, cancel = context.WithCancel(context.Background())
 				wg.Add(1)
 				go draw(ctx, conn)
 			} else {
-                fmt.Print("Stopping flooding...")
+				fmt.Print("Stopping flooding...")
 				cancel()
 			}
 		}
@@ -77,7 +87,7 @@ func getModeFromCommanderer() floodMode {
 	response, err := http.Get("http://localhost:9000/mode")
 	if err != nil {
 		fmt.Print(err.Error())
-        // TODO: error handling
+		// TODO: error handling
 	}
 
 	responseData, err := io.ReadAll(response.Body)
@@ -85,35 +95,76 @@ func getModeFromCommanderer() floodMode {
 		log.Fatal(err)
 	}
 
-    // parse the response
-    var mode floodMode
+	// parse the response
+	var mode floodMode
 	err = json.Unmarshal([]byte(string(responseData)), &mode)
 	if err != nil {
 		fmt.Println("Error:", err)
-        // TODO: error handling
+		// TODO: error handling
 	}
-    return mode
+	return mode
+}
+
+func readImage(filename string) floodImage {
+	imgFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		// TODO: clarify how to do proper error handling here
+		return floodImage{}
+	}
+	defer imgFile.Close()
+
+	// decode the image
+	img, _, err := image.Decode(imgFile)
+	if err != nil {
+		fmt.Println("Error decoding image:", err)
+		// TODO: clarify how to do proper error handling here
+		return floodImage{}
+	}
+
+	widthPX := img.Bounds().Dx()
+	heightPX := img.Bounds().Dy()
+
+	var rgbValues []string
+
+	for y := 0; y < heightPX; y++ {
+		for x := 0; x < widthPX; x++ {
+			// Get the color of the pixel at (x, y)
+			r, g, b, _ := img.At(x, y).RGBA()
+
+			// Convert to 8-bit RGB
+			r8 := uint8(r >> 8)
+			g8 := uint8(g >> 8)
+			b8 := uint8(b >> 8)
+
+			buf := []uint8{r8, g8, b8}
+
+			rgb := hex.EncodeToString(buf)
+			rgbValues = append(rgbValues, rgb)
+		}
+	}
+	return floodImage{HeightPX: heightPX, WidthPX: widthPX, Bytes: rgbValues}
 }
 
 func draw(ctx context.Context, conn net.Conn) {
 	for {
-		color := generateColor()
+		img := readImage("image.png")
 		select {
 		case <-ctx.Done(): // if cancel() execute
 			wg.Done()
 			return
 		default:
-			for i := 0; i <= 500; i++ {
-				cmd := fmt.Sprintf("PX %d %d %s\n", i, i, color)
-				_, err := conn.Write([]byte(cmd))
-				if err != nil {
-					fmt.Println(err)
-					wg.Done()
-					return
+			for y := 0; y < img.HeightPX; y++ {
+				for x := 0; x < img.WidthPX; x++ {
+					cmd := fmt.Sprintf("PX %d %d %s\n", y, x, img.Bytes[y*img.WidthPX+x])
+					_, err := conn.Write([]byte(cmd))
+					if err != nil {
+						fmt.Println(err)
+						wg.Done()
+						return
+					}
 				}
 			}
-			// TODO: flood all the time and get the color from a generator
-			time.Sleep(1 / 4 * time.Second)
 		}
 	}
 }
