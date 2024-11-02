74b7e8f7937d12528753c8815b6698ac4e93efcf
Ruben Hoenle <git@hoenle.xyz>
2024-11-02 02:10:09 +0100

    fix(pixelknecht): ignore black color when painting
    the transparent background of png files should be ignored now
    
    will ignore all black colors, so use alpha layer in the future

--- pixelknecht/tcp_output.go
+++ pixelknecht/tcp_output.go
@@ -5,13 +5,13 @@ import (
 	"net"
 )
 
-const workerPoolSize = 20
+const workerPoolSize = 200
 
 func initTcpWorkerPool() {
 	for i := 0; i < workerPoolSize; i++ {
 		wg.Add(1)
 		go func() {
-			conn, err := net.Dial("tcp", "localhost:1337")
+			conn, err := net.Dial("tcp", "129.143.235.135:1234")
 			if err != nil {
 				fmt.Println(err)
 				return
